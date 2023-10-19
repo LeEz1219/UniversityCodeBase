6172a7d3b77fa5e07d772eadf7dd5bb907cf3fb4
LeEz1219 <61774231+LeEz1219@users.noreply.github.com>
2023-10-19 01:16:54 -0700

    Add files via upload

---
+++ CSE461OperatingSystems/lab2/sdisk.cpp
@@ -0,0 +1,80 @@
+#include "sdisk.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+/*
+Class Sdisk
+{
+public :
+Sdisk(string diskname, int numberofblocks, int blocksize);
+int getblock(int blocknumber, string& buffer);
+int putblock(int blocknumber, string buffer);
+int getnumberofblocks(); // accessor function
+int getblocksize(); // accessor function
+private :
+string diskname;        // file name of software-disk
+int numberofblocks;     // number of blocks on disk
+int blocksize;          // block size in bytes
+};
+*/
+
+
+Sdisk::Sdisk(string diskname, int numberofblocks, int blocksize){
+	this->diskname = diskname;
+	this->numberofblocks = numberofblocks;
+	this->blocksize = blocksize;
+	ifstream ifs;
+	ifs.open(diskname.c_str(), ios::in);
+	char x = ifs.get();
+	if(ifs.good()){
+		cout << diskname << " disk exists." << endl;
+		ifs.close();
+	}else{
+		ifs.close();
+		ofstream ofs;
+		ofs.open(diskname.c_str(), ios::out);
+		for(int i = 1; i <= (blocksize * numberofblocks); ++i){
+			ofs << "#";
+		}
+		ofs.close();
+	}
+}
+
+int Sdisk::getblock(int blocknumber, string& buffer){
+	if(blocknumber > this->numberofblocks) return 0;
+	ifstream ifs;
+	ifs.open(this->diskname, ios::in);
+	ifs.seekg(blocknumber * getblocksize());
+	char c;
+	for(int i = 0; i < getblocksize(); ++i){
+		ifs.get(c);
+		buffer += c;
+	}
+	ifs.close();
+	return 1;
+}
+
+int Sdisk::putblock(int blocknumber, string buffer){
+	if(blocknumber > this->numberofblocks) return 0;
+	ofstream ofs;
+	ofs.open(this->diskname.c_str(), ios::out | ios::in);
+	ofs.seekp((blocknumber * blocksize));
+	for(int i = 0; i < blocksize && i < buffer.length(); ++i){
+		ofs.put(buffer[i]);
+	}
+	ofs.close();
+	return 1;
+}
+
+int Sdisk::getnumberofblocks(){ // accessor function
+	return this->numberofblocks;
+}
+
+int Sdisk::getblocksize(){ // accessor function
+	return this->blocksize;
+}
+
